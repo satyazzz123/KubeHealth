43fad90a9231d09ec5f228b31d859dd511227b6d
satyazzz123 <beherasatyajit716@gmail.com>
2023-11-18 01:25:01 +0530

    added functionality to view details of resources
    Signed-off-by: satyazzz123 <beherasatyajit716@gmail.com>

---
+++ cmd/details/details.go
@@ -0,0 +1,32 @@
+package details
+
+import (
+	"log"
+
+	"github.com/Parthiba-Hazra/clstres/cmd"
+	"github.com/spf13/cobra"
+)
+
+// detailsCmd represents the details command
+var DetailsCmd = &cobra.Command{
+	Use:   "details",
+	Short: "show kubernetes resources details",
+	Long:  `It show a detail view of a kubernetes resoureces according to user input`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Print("please provide resource type to get details")
+	},
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(DetailsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// detailsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// detailsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/details/service/service.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/satyazzz123/KubeHealth/client"
+	"github.com/satyazzz123/KubeHealth/details"
+	"github.com/satyazzz123/KubeHealth/helper"
+	"github.com/spf13/cobra"
+)
+
+// serviceCmd represents the service command
+var serviceCmd = &cobra.Command{
+	Use:   "service",
+	Short: "Get details of a Kubernetes service",
+	Long: `Retrieve detailed information about a Kubernetes service, including its name, namespace,
+creation time, labels, type, cluster IP, external IPs, load balancer IP, ports, selector,
+and session affinity.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		namespace, _ := cmd.Flags().GetString("ns")
+		serviceName, _ := cmd.Flags().GetString("s")
+
+		client, err := client.GetClient()
+		if err != nil {
+			log.Printf("error getting kubernetes client: %v", err)
+		}
+		serviceDetailsList, err := helper.ShowServiceDetails(client, namespace, serviceName)
+		if err != nil {
+			log.Printf("error getting service details: %v", err)
+		} else {
+			for _, service := range serviceDetailsList {
+				fmt.Println("Service Name:", service.Name)
+				fmt.Println("Namespace:", service.Namespace)
+				fmt.Println("Creation Time:", service.CreationTime)
+				fmt.Println("Labels:", service.Labels)
+				fmt.Println("Type:", service.Type)
+				fmt.Println("Cluster IP:", service.ClusterIP)
+				fmt.Println("External IPs:", service.ExternalIPs)
+				fmt.Println("LoadBalancer IP:", service.LoadBalancerIP)
+
+				fmt.Println("Ports:")
+				for _, port := range service.Ports {
+					fmt.Println("  - Name:", port.Name)
+					fmt.Println("    Protocol:", port.Protocol)
+					fmt.Println("    Port:", port.Port)
+					fmt.Println("    Target Port:", port.TargetPort)
+					fmt.Println("    Node Port:", port.NodePort)
+				}
+
+				fmt.Println("Selector:", service.Selector)
+				fmt.Println("Session Affinity:", service.SessionAffinity)
+
+				fmt.Println("---------------------------")
+			}
+		}
+	},
+}
+
+func init() {
+	details.DetailsCmd.AddCommand(serviceCmd)
+	serviceCmd.PersistentFlags().String("s", "", "You need to provide the name of pod in order to get details of that perticular pod (eg: --s=service-name)")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serviceCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serviceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
